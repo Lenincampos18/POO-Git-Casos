a3f1d93eb867a288eb65ba336bb666220d56b3e5
Lenin Campos <jcampos1810@gmail.com>
2025-10-21 21:49:48 -0500

    Primer Commit: Ejercicio Un Numero e Indicar si es Positivo,Negativo o Cero

---
+++ Positivo-Negativo-Cero.py
@@ -0,0 +1,12 @@
+#Ingresar un Número y mostrar si es Positivo , Negativo o Cero
+def Positivo_negativo_cero():
+    n=float(input("Ingrese Número:"))
+    if (n<0):
+        print("El Número es Negativo")
+    elif (n>0):
+        print("El Número es Positivo")
+    else:
+        print("El Número es 0")
+
+#Invocación de Métodos
+Positivo_negativo_cero()
